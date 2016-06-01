eb9d00ee4d7166f78237df8ab38fd461a2187a17
Will Grange <will.grange@gmail.com>
2016-06-01 18:10:05 +0100

    Rules unmodelled

--- decimalToRomanNumerals.spec.js
+++ decimalToRomanNumerals.spec.js
@@ -31,6 +31,48 @@ describe("translator", () => {
     })
   })
 
+  describe("given 4", () => {
+    it("returns numerals IV", () => {
+      let translatedValue = translator.translate(4)
+      translatedValue.should.equal("IV")
+    })
+  })
+
+  describe("given 5", () => {
+    it("returns numerals V", () => {
+      let translatedValue = translator.translate(5)
+      translatedValue.should.equal("V")
+    })
+  })
+
+  describe("given 6", () => {
+    it("returns numerals VI", () => {
+      let translatedValue = translator.translate(6)
+      translatedValue.should.equal("VI")
+    })
+  })
+
+  describe("given 7", () => {
+    it("returns numerals VII", () => {
+      let translatedValue = translator.translate(7)
+      translatedValue.should.equal("VII")
+    })
+  })
+
+  describe("given 8", () => {
+    it("returns numerals VIII", () => {
+      let translatedValue = translator.translate(8)
+      translatedValue.should.equal("VIII")
+    })
+  })
+
+  describe("given 9", () => {
+    it("returns numerals IX", () => {
+      let translatedValue = translator.translate(9)
+      translatedValue.should.equal("IX")
+    })
+  })
+
 
 })
 
@@ -38,10 +80,35 @@ class Translator{
   translate(integer) {
 
     let numerals = ''
-    for (let i = 0; i < integer; i++) {
-        numerals = numerals + 'I'
+    if (integer < 4) {
+      for (let i = 0; i < integer; i++) {
+          numerals = numerals + 'I'
+      }
+      return numerals
     }
-    
+
+    if (integer == 4) {
+      return 'IV'
+    }
+
+    if (integer == 5) {
+      return 'V'
+    }
+
+    if (integer == 9) {
+      return 'IX'
+    }
+
+    if (integer > 5) {
+      numerals = 'V'
+      for (let i = 0; i < integer - 5; i++) {
+          numerals = numerals + 'I'
+      }
+
+      return numerals
+    }
+
+
     return numerals
   }
 }
