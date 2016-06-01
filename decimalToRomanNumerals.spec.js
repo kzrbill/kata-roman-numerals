699e911625167f081532c01c4937a9390f85b0ec
Will Grange <will.grange@gmail.com>
2016-06-01 17:58:33 +0100

    Dealing with 1-4

--- decimalToRomanNumerals.spec.js
+++ decimalToRomanNumerals.spec.js
@@ -11,21 +11,37 @@ describe("translator", () => {
   })
 
   describe("given 1", () => {
-
-    let translatedValue
-
-    beforeEach(() => {
-      translatedValue = translator.translate(1)
-    })
-
     it("returns numerals I", () => {
+      let translatedValue = translator.translate(1)
       translatedValue.should.equal("I")
     })
   })
+
+  describe("given 2", () => {
+    it("returns numerals II", () => {
+      let translatedValue = translator.translate(2)
+      translatedValue.should.equal("II")
+    })
+  })
+
+  describe("given 3", () => {
+    it("returns numerals III", () => {
+      let translatedValue = translator.translate(3)
+      translatedValue.should.equal("III")
+    })
+  })
+
+
 })
 
 class Translator{
-  translate() {
-    return 'I'
+  translate(integer) {
+
+    let numerals = ''
+    for (let i = 0; i < integer; i++) {
+        numerals = numerals + 'I'
+    }
+    
+    return numerals
   }
 }
